ef507552e92119b451153d3af7a9ca2aaff92bef
hngskj <hngskj@gmail.com>
2019-11-04 12:51:09 +0900

    Add new lsa from excel

--- lsa_default.py
+++ lsa_default.py
@@ -1,7 +1,6 @@
 # REFERENCE
 # https://medium.com/@adriensieg/text-similarities-da019229c894
 # http://mccormickml.com/2016/11/04/interpreting-lsi-document-similarity/?source=post_page-----da019229c894----------------------
-# https://colab.research.google.com/drive/11tj__mAorAbsS2VkeClMu5gN2wahqRSF?source=post_page-----da340774ce23----------------------#scrollTo=TS1RAtEv1tLR&forceEdit=true&sandboxMode=true
 
 
 import numpy as np
--- lsa_xlsx.py
+++ lsa_xlsx.py
@@ -7,6 +7,7 @@ file_path = 'sample.xlsx'
 data = pd.read_excel(file_path)
 
 groups = ['group1', 'group2', 'group3', 'group4', 'group5', 'group6']
+values = ['value1', 'value2', 'value3', 'value4', 'value5', 'value6']
 words = []
 for g in groups:
     words.append(data[g].values.tolist())
@@ -15,15 +16,18 @@ for g in groups:
 dictionary = gensim.corpora.Dictionary(words)
 word_dict = dict(dictionary)
 word_dict_len = len(word_dict)
-print("\nDICTIONARY")
-pprint(word_dict)
+# print("\nDICTIONARY")
+# pprint(word_dict)
 
 
 corpus = [dictionary.doc2bow(word) for word in words]
-print("\nCORPUS")
+# print("\nCORPUS")
 # pprint(corpus)
 
 
 mat_a = gensim.matutils.corpus2dense(corpus, word_dict_len)
-print("\nMATRIX A")
-pprint(mat_a)
+# print("\nMATRIX A")
+# pprint(mat_a)
+
+
+print(data.loc[data['group1']=='art', ['value1']])
